c959af95901603cc0a537273bd2b419cebbfe7cc
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2017-04-23 21:00:47 +0200

    Create L298N.h

---
+++ L298N/L298N.h
@@ -0,0 +1,22 @@
+#ifndef L298N_h
+#define L298N_h
+
+class RobotMovil {
+  public:  
+    L298N (int IN1, int IN2, int IN3, int IN4, int ENA, int ENB);
+    
+    void forward (int velocidad_L, int velocidad_R, int tiempo);
+    void reverse (int velocidad_L, int velocidad_R, int tiempo);
+    void drive (int velocidad_L, int velocidad_R, int tiempo);
+    void rotate (int tiempo);
+    void brake (int tiempo);
+  
+    int _IN1;
+    int _IN2;
+    int _IN3;
+    int _IN4;
+    int _ENA;
+    int _ENB;
+
+};
+#endif
