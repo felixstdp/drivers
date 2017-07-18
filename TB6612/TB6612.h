8178f1dcbf5da62f435eeace1f9ae9978b3b890a
Félix Sánchez-Tembleque <fstdp@hotmail.com>
2017-07-18 09:51:11 +0200

    Update TB6612.h

--- TB6612/TB6612.h
+++ TB6612/TB6612.h
@@ -3,12 +3,12 @@
 
 class TB6612 {
   public:  
-    TB6612 (int AIN1, int AIN2, int BIN1, int BIN2, int PWMA, int PWMB, boolean STBY);
+    TB6612 (int AIN1, int AIN2, int BIN1, int BIN2, int PWMA, int PWMB, int STBY);
     
     void forward (int velocidad_L, int velocidad_R, int tiempo);
     void reverse (int velocidad_L, int velocidad_R, int tiempo);
     void drive (int velocidad_L, int velocidad_R, int tiempo);
-    void rotate (int velcocidad, int tiempo);
+    void rotate (int velocidad, int tiempo);
     void brake (int tiempo);
   
     int _AIN1;
@@ -17,7 +17,7 @@ class TB6612 {
     int _BIN2;
     int _PWMA;
     int _PWMB;
-    boolean _STBY;
+    int _STBY;
 
 };
 #endif
